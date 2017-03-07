34cb77a1412f0a0e91ac60f1a0500c852e6e8381
Mark deGroat <mhdegroat@gmail.com>
2017-03-07 14:32:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array.first
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    string_to_return = ""
+    array.each do |element|
+      if element == array.last
+        string_to_return << "and #{element}"
+      elsif element != array.first
+        string_to_return << "#{element}, "
+      else
+        string_to_return << "#{element}, "
+      end
+    end
+    return string_to_return
+
+  end
+
+
 
 end
